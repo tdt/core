fb0b452db12d8ee665132640c6aa8c80f5a4997d
Miel Vander Sande <miel.vandersande@ugent.be>
2013-01-14 18:32:52 +0100

    Fixed SPARQL onAdd()

--- strategies/SPARQL.class.php
+++ strategies/SPARQL.class.php
@@ -58,18 +58,29 @@ class SPARQL extends CSV {
         /**
          * check if the endpoint is valid ( not empty )
          */
-        if (!isset($configObject->endpoint))
-            throw new TDTException(452, array("Can't find endpoint for executing SPARQL"));
+//        if (!isset($configObject->endpoint))
+//            throw new TDTException(452, array("Can't find endpoint for executing SPARQL"));
+//
+//        if (!isset($configObject->query))
+//            throw new TDTException(452, array("No SPARQL query supplied"));
+//
+//        $configObject->uri = $configObject->endpoint . '?query=' . urlencode($configObject->query) . '&format=csv';
+//        $configObject->has_header_row = "1";
+//        $configObject->delimiter = ",";
+//        $configObject->start_row = "1";
 
-        if (!isset($configObject->query))
-            throw new TDTException(452, array("No SPARQL query supplied"));
-        
-        $configObject->uri = $configObject->endpoint . '?query=' . urlencode($configObject->query) . '&format=csv';
-        $configObject->has_header_row = "1";
-        $configObject->delimiter = ",";
-        $configObject->start_row = "1";
-        
         return parent::read($configObject, $package, $resource);
+    }
+
+    public function onAdd($package_id, $gen_resource_id) {
+        if (!isset($this->uri)) {
+            $this->uri = $this->endpoint . '?query=' . urlencode($this->query) . '&format=csv';
+            ;
+        }
+        if (!isset($this->delimiter)) {
+            $this->delimiter = ",";
+        }
+        parent::onAdd($package_id, $gen_resource_id);
     }
 
 }
