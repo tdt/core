f9a7da2e3f8b2f0d43b35cca6ddf0b6cfe6ca1da
Miel Vander Sande <miel.vandersande@ugent.be>
2013-01-14 17:37:33 +0100

    Fixed start row of SPARQL strategy

--- strategies/SPARQL.class.php
+++ strategies/SPARQL.class.php
@@ -67,6 +67,7 @@ class SPARQL extends CSV {
         $configObject->uri = $configObject->endpoint . '?query=' . urlencode($configObject->query) . '&format=csv';
         $configObject->has_header_row = "1";
         $configObject->delimiter = ",";
+        $configObject->start_row = "2";
         
         return parent::read($configObject, $package, $resource);
     }
