63c4cae4d8e3c872c767b8bec8a83c2b8afa35ef
Pieter Colpaert <pieterc@member.fsf.org>
2013-01-15 21:58:45 +0100

    SPARQL added

---
+++ src/tdt/core/strategies/JSON.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * An abstract class for JSON data
+ *
+ * @package The-Datatank/model/resources/strategies
+ * @copyright (C) 2011 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Jan Vansteenlandt
+ */
+
+namespace tdt\core\strategies;
+
+use tdt\core\model\read\AResourceStrategy;
+use tdt\framework\Log;
+use tdt\framework\TDTException;
+
+class JSON extends AResourceStrategy{
+    
+    public function read(&$configObject,$package,$resource){ 
+        $data = \tdt\framework\Request::http($configObject->uri);
+        return json_decode($data->data);
+    }
+
+    public function isValid($package_id,$generic_resource_id){
+        $data = \tdt\framework\Request::http($this->uri);
+        $result = json_decode($data->data);
+        if($result != true){
+            throw new ResourceAdditionTDTException("Could not transfrom the json data from ". $this->uri ." to a php object model, please check if the json is valid.");
+        }
+        return true;
+    }
+    
+    // @deprecated
+    public function onUpdate($package, $resource){
+        
+    }
+
+    public function documentCreateRequiredParameters(){
+        return array("uri");
+    }
+    
+    public function documentReadRequiredParameters(){
+        return array();
+    }
+    
+
+    public function documentUpdateRequiredParameters(){
+        return array();
+    }
+    
+
+   public function documentCreateParameters(){
+       return array(
+           "uri" => "The uri to the json document."
+       );  
+   }
+   
+   public function documentReadParameters(){
+       return array();
+   }
+   
+   public function documentUpdateParameters(){
+       return array();
+   }
+
+   public function getFields($package,$resource){
+       return array();
+   }
+
+}
