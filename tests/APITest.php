b99cea44000a139a16dbc75257e0e1d3ef388db5
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2013-01-24 17:14:15 +0100

    Added a database MySQL test.

--- tests/APITest.php
+++ tests/APITest.php
@@ -54,8 +54,7 @@ class APITest extends \PHPUnit_Framework_TestCase {
         try{
             $model = ResourcesModel::getInstance();
             $model->createResource($TEST_PACKAGE_NAME . "/" . $TEST_RESOURCE_NAME,$parameters); 
-        }catch(Exception $ex){
-            var_dump($ex->getTrace());
+        }catch(Exception $ex){            
             $create_resource = false;
         }
         
@@ -77,7 +76,7 @@ class APITest extends \PHPUnit_Framework_TestCase {
             $this->assertEquals("Clayton",$object1->Name);                   
             $this->assertEquals("Ap #630-7719 Scelerisque Road",$object1->Street_Address);
             
-        }catch(Exception $ex){            
+        }catch(Exception $ex){                   
             $read_datasource = false;
         }
 
@@ -90,13 +89,80 @@ class APITest extends \PHPUnit_Framework_TestCase {
         try{
             $model = ResourcesModel::getInstance();
             $model->deleteResource($TEST_PACKAGE_NAME, $TEST_RESOURCE_NAME,array());
-        }catch(Exception $ex){
+        }catch(Exception $ex){            
             $delete_datasource = false;
         }
 
         $this->assertTrue($delete_datasource);
+    }
 
+    public function testDB(){
 
+        $TEST_PACKAGE_NAME = "UNITTESTDB";
+        $TEST_RESOURCE_NAME = "db1";
+
+        /*
+         * Pass along the database parameters leading to the test data
+         */
+        $parameters = array(
+            'documentation' => "This is a test case for unittesting a DB resource.",
+            'resource_type' => "generic/DB",
+            'db_table' => "test_table",
+            'location' => "localhost",
+            'username' => "root",
+            'password' => "",
+            'db_name' => "myapp_test",
+            'db_type' => "MySQL"
+
+        );    
+        
+        /*
+         * Try creating a resource, if anything fails, the test fails
+         */
+        $create_resource = true;
+        try{
+            $model = ResourcesModel::getInstance();
+            $model->createResource($TEST_PACKAGE_NAME . "/" . $TEST_RESOURCE_NAME,$parameters); 
+        }catch(Exception $ex){           
+            $create_resource = false;
+        }
+        
+        $this->assertTrue($create_resource);
+        
+        /*
+         * Try reading the datasource
+         */
+        $read_datasource = true;
+        try{
+            $model = $model = ResourcesModel::getInstance();
+            $db_object = $model->readResource($TEST_PACKAGE_NAME,$TEST_RESOURCE_NAME,array(),array());
+
+            // Lets get the first rowobject and compare some values.
+            // Clayton;Ap #630-7719 Scelerisque Road;ac.arcu@facilisismagna.ca;Pierre
+            $object1 = array_shift($db_object);
+            $this->assertEquals("Yoshi",$object1->Name);
+            $this->assertEquals("Weber",$object1->Surname);
+            $this->assertEquals("Lakewood",$object1->City);
+            $this->assertEquals("6435",$object1->Income);
+            
+        }catch(Exception $ex){                     
+            $read_datasource = false;
+        }
+
+        $this->assertTrue($read_datasource);
+
+        /*
+         * Try deleting the resource
+         */
+        $delete_datasource = true;
+        try{
+            $model = ResourcesModel::getInstance();
+            $model->deleteResource($TEST_PACKAGE_NAME, $TEST_RESOURCE_NAME,array());
+        }catch(Exception $ex){           
+            $delete_datasource = false;
+        }
+
+        $this->assertTrue($delete_datasource);
     }
 
 }
