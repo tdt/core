872df68be8afa0436ef5b96ed666cf5f55affe04
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2013-04-18 16:43:19 +0200

    Namespacing in the sort execution, and max execution. Issue #34.

--- src/tdt/core/universalfilter/interpreter/executers/implementations/MaxAggregatorExecuter.php
+++ src/tdt/core/universalfilter/interpreter/executers/implementations/MaxAggregatorExecuter.php
@@ -10,7 +10,16 @@ class MaxAggregatorExecuter extends AggregatorFunctionExecuter {
 
     public function calculateValue(UniversalFilterTableContent $column, $columnId) {
         $data = $this->convertColumnToArray($column, $columnId);
-        return max($data);
+        // Instead of using the max() function of php
+        // We'll filter our own maximum, numbers can be encapsulated as string
+        // and if a "null" value is present, in an array of string-integers, null will be
+        // the max.
+        $max = "";
+        foreach($data as $entry){
+            if($entry > $max && $entry != "null")
+                $max = $entry;
+        }
+        return $max;
     }
 
     public function keepFullInfo() {
