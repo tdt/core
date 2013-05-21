286abd94b1c3dce0720f6f2ccbdb21e62e339061
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2013-05-21 14:15:00 +0200

    Issue #45.

--- src/tdt/core/controllers/CUDController.php
+++ src/tdt/core/controllers/CUDController.php
@@ -197,7 +197,7 @@ class CUDController extends AController {
     }
 
     /**
-     * PATCH is a 'new' request HTTP HEADER which allows to update a piece of a definition of a resource in our context
+     * PATCH is a relatively new request HTTP HEADER which will be used to update a piece of a resource definition.
      */
     public function PATCH($matches) {
 
@@ -212,11 +212,6 @@ class CUDController extends AController {
 
         $RESTparameters = array();
 
-        /**
-         * Since we do not know where the package/resource/requiredparameters end, we're going to build the package string
-         * and check if it exists, if so we have our packagestring. Why is this always correct ? Take a look at the
-         * ResourcesModel class -> funcion isResourceValid()
-         */
         $foundPackage = FALSE;
         $resourcename = "";
         $reqparamsstring = "";
@@ -268,7 +263,21 @@ class CUDController extends AController {
 
         // patch (array) contains all the patch parameters
         $patch = array();
-        parse_str(file_get_contents("php://input"), $patch);
+        $HTTPheaders = getallheaders();
+        if (isset($HTTPheaders["Content-Type"]) && $HTTPheaders["Content-Type"] == "application/json") {
+            $json_string = file_get_contents("php://input");
+            $patch = json_decode($json_string,true);
+
+            // Check if the object is wrapped or not (e.g. are the parameters already in the object, or are these wrapped.)
+            $param_object = array_shift($patch);
+            if(is_array($param_object)){
+                $patch = $param_object;
+            }else{
+                $patch = json_decode($json_string,true);
+            }
+        } else {
+            parse_str(file_get_contents("php://input"), $patch);
+        }
 
         $model = ResourcesModel::getInstance(Config::getConfigArray());
         $model->updateResource($package, $resourcename, $patch, $RESTparameters);
@@ -290,5 +299,3 @@ class CUDController extends AController {
     }
 
 }
-
-?>
