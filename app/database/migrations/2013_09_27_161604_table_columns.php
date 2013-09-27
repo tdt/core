1c83de53a9cc76b1a4bf43d25c27c5252674a32a
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2013-09-27 18:34:06 +0200

    Migration for tabular columns

---
+++ app/database/migrations/2013_09_27_161604_table_columns.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class TableColumns extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create the table for the tabular columns model.
+        Schema::create('tabularcolumns', function($table){
+
+            $table->increments('id');
+            $table->integer('index');
+            $table->string('column_name', 255);
+            $table->boolean('is_pk');
+            $table->string('column_name_alias');
+            $table->integer('tabular_type');
+            $table->integer('tabular_int');
+
+            // created_at | updated_at DATETIME, are default expected by the Eloquent ORM.
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Drop the tabular columns table.
+		Schema::drop('tabularcolumns');
+	}
+
+}
