340de09a76352961039f8339d12b65bf6add880e
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2013-09-29 23:25:15 +0200

    Added service provider and input package.

--- app/config/app.php
+++ app/config/app.php
@@ -113,6 +113,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		//'Tdt\Input\InputServiceProvider',
 
 	),
 
