5a7110e1420e4aeb291c6aea51e0935549fd512f
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2013-10-02 18:16:04 +0200

    Added input to the serviceproviders.

--- app/config/app.php
+++ app/config/app.php
@@ -113,7 +113,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		//'Tdt\Input\InputServiceProvider',
+		'tdt\input\InputServiceProvider',
 
 	),
 
