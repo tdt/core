d94d7ae80ac8f6b7d4fabac00777ebd145bc6109
Ntynmichiel <michielvancoillie@gmail.com>
2013-10-28 18:30:32 +0100

    Update abstract class

--- app/controllers/tdt/core/datacontrollers/ADataController.php
+++ app/controllers/tdt/core/datacontrollers/ADataController.php
@@ -13,7 +13,7 @@ abstract class ADataController {
 
     protected static $DEFAULT_PAGE_SIZE = 312;
 
-    public abstract function readData($source_definition, $parameters = null);
+    public abstract function readData($source_definitions);
 
     /**
      * Calculate the limit and offset based on the request string parameters.
