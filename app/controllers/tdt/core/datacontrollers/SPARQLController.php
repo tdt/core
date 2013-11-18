10ab859c54829e754b6c4214ce2d25be4180bb18
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2013-11-18 16:06:07 +0100

    Sparql can now handle parametrized queries, also tested with array parameters

--- app/controllers/tdt/core/datacontrollers/SPARQLController.php
+++ app/controllers/tdt/core/datacontrollers/SPARQLController.php
@@ -26,6 +26,9 @@ class SPARQLController extends ADataController {
         $endpoint_password = $source_definition->endpoint_password;
         $query = $source_definition->query;
 
+        // Process the parameters in the uri (to catch hashtag values for example)
+        $query = $this->processParameters($query);
+
         // Create a count query for paging purposes, this assumes that a where clause is included in the query
         // Note that the where "clause" is obligatory but it's not mandatory it is preceded by a WHERE keyword
         $matches = array();
@@ -59,12 +62,13 @@ class SPARQLController extends ADataController {
         $query = $matches[1];
 
         // Prepare the query to count results
-        $count_query = 'SELECT count(?s) AS ?count ' . $query;
+        $count_query = 'SELECT count(*) AS ?count ' . $query;
 
         $count_query = urlencode($count_query);
         $count_query = str_replace("+", "%20", $count_query);
 
         $count_uri = $endpoint . '?query=' . $count_query . '&format=' . urlencode("application/rdf+xml");
+
         $response = $this->executeUri($count_uri, $endpoint_user, $endpoint_password);
 
         // Parse the triple response and retrieve the form them containing our count result
@@ -76,6 +80,7 @@ class SPARQLController extends ADataController {
         // Get the results#value, in order to get a count of all the results
         // This will be used for paging purposes
         $count = 0;
+
         foreach ($triples as $triple){
             if(!empty($triple['p']) && preg_match('/.*sparql-results#value/',$triple['p'])){
                 $count = $triple['o'];
@@ -86,6 +91,7 @@ class SPARQLController extends ADataController {
         $paging = $this->calculatePagingHeaders($limit, $offset, $count);
 
         $query = $source_definition->query;
+        $query = $this->processParameters($query);
 
         if(!empty($offset)){
             $query = $query . " OFFSET $offset ";
@@ -96,12 +102,14 @@ class SPARQLController extends ADataController {
         }
 
         // Prepare the query with proper encoding for the request
+
         $q = urlencode($query);
         $q = str_replace("+", "%20", $q);
 
         if($keyword == 'select'){
 
             $query_uri = $endpoint . '?query=' . $q . '&format=' . urlencode("application/sparql-results+json");
+
             $response = $this->executeUri($query_uri, $endpoint_user, $endpoint_password);
             $result = json_decode($response);
 
@@ -147,6 +155,7 @@ class SPARQLController extends ADataController {
 
         // If credentials are given, put the HTTP auth header in the cURL request
         if(!empty($user)){
+
             curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
             curl_setopt($ch, CURLOPT_USERPWD, $user . ":" . $password);
         }
@@ -169,7 +178,7 @@ class SPARQLController extends ADataController {
 
         // According to the SPARQL 1.1 spec, a SPARQL endpoint can only return 200,400,500 reponses
         if($response_code == '400'){
-            \App::abort(452, "The SPARQL endpoint returned a 400 error.");
+            \App::abort(452, "The SPARQL endpoint returned a 400 error. If the SPARQL query contained a parameter, don't forget to pass them as a query string parameter.");
         }else if($response_code == '500'){
             \App::abort(452, "The SPARQL endpoint returned a 500 error.");
         }
@@ -177,4 +186,104 @@ class SPARQLController extends ADataController {
 
         return $response;
     }
+
+    /**
+     * Replace parameters in the SPARQL query with the corresponding
+     * passed query parameters.
+     */
+    private function processParameters($query) {
+
+        $parameters = \Request::all();
+
+        $placeholders = array();
+
+        preg_match_all("/\\$\\{(.+?)\\}/", $query, $placeholders, PREG_SET_ORDER);
+
+        for ($i = 0; $i < count($placeholders); $i++) {
+
+            $placeholder = trim($placeholders[$i][1]);
+
+            $elements = array();
+            //For example ${x.each('?t = $_','||')}
+            preg_match_all("/([a-zA-Z]+?)\\.([a-zA-Z]+?)\\('(.*?)','(.*?)'\\)/", $placeholder, $elements, PREG_SET_ORDER);
+
+            if (!empty($elements)){
+                $placeholder = trim($elements[0][1]);
+            }
+
+            if (!empty($elements[0]) && count($elements[0]) > 0 && count($elements[0]) != 5)
+                \App::abort(400, "The added placeholder is malformed");
+
+            if (empty($elements)) {
+                //${name[0]}
+                $index = strpos($placeholder, "[");
+
+                if ($index !== false) {
+                    $placeholder_name = substr($placeholder,0, $index);
+                    $placeholder_index = substr($placeholder, $index + 1, -1);
+
+                    if (!isset($parameters[$placeholder_name]))
+                        \App::abort(400, "The parameter $placeholder_name was not provided");
+
+
+                    if (!isset($parameters[$placeholder_name][$placeholder_index]))
+                        \App::abort(400, "The index $placeholder_index of parameter $placeholder does not exist.");
+
+                    $value = $parameters[$placeholder_name][$placeholder_index];
+                } else {
+
+                    if (!isset($parameters[$placeholder]))
+                        \App::abort(400, "The parameter $placeholder was not provided");
+
+                    $value = $parameters[$placeholder];
+
+                    if (is_array($value))
+                        \App::abort(400, "The parameter $placeholder is single value, array given.");
+                }
+                $value = addslashes($value);
+                $value = urldecode($value);
+
+                $query = str_replace("\${" . $placeholder . "}", $value, $query);
+                continue;
+            }
+
+            $function = trim($elements[0][2]);
+            $pattern = trim($elements[0][3]);
+            $concat = trim($elements[0][4]);
+
+            $replacement = $this->processParameterFunction($parameters[$placeholder], $function, $pattern, $concat);
+
+            $placeholder = "\${" . $elements[0][0] . "}";
+
+            $query = str_replace($placeholder, $replacement, $query);
+
+        }
+
+        return $query;
+    }
+
+    private function processParameterFunction($values, $function, $pattern, $concat) {
+
+        $result = null;
+
+        switch ($function) {
+            case "each":
+            if (!is_array($values))
+                $values = array($values);
+
+            $arr_result = array();
+            foreach ($values as $value)
+                $arr_result[] = str_replace("\$_", "\"$value\"", $pattern);
+
+            $result = implode($concat, $arr_result);
+
+            break;
+
+            default:
+            \App::abort(400, "Unknown placeholder function $function");
+        }
+
+        return $result;
+    }
+
 }
