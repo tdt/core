8cf433e3ba2f78585a9cc4a57ee78c5c86e58cf2
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2014-02-21 17:09:06 +0100

    Issue #173

---
+++ app/controllers/tdt/core/formatters/JSONLDFormatter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace tdt\core\formatters;
+
+/**
+ * JSON-LD Formatter
+ * @copyright (C) 2011,2013 by OKFN Belgium vzw/asbl
+ * @license AGPLv3
+ * @author Jan Vansteenlandt <jan@okfn.be>
+ */
+class JSONLDFormatter implements IFormatter{
+
+    public static function createResponse($dataObj){
+
+        // Create response
+        $response = \Response::make(self::getBody($dataObj), 200);
+
+        // Set headers
+        $response->header('Content-Type', 'application/ld+json;charset=UTF-8');
+
+        return $response;
+    }
+
+    public static function getBody($dataObj){
+
+        if($dataObj->is_semantic){
+
+            // Check if a configuration is given
+            $conf = array();
+            if(!empty($dataObj->semantic->conf)){
+                $conf = $dataObj->semantic->conf;
+            }
+
+            return $dataObj->data->serialise('jsonld');
+        }else{
+            \App::abort(400, "The data is not a semantically linked document, a linked data JSON representation is not possible.");
+        }
+
+    }
+
+    public static function getDocumentation(){
+        return "A JSON-LD formatter.";
+    }
+
+}
