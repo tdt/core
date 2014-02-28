040be677596f6baf0d78909d8f56b638cceea043
Michiel Vancoillie <info@ninetynine.be>
2014-02-28 14:41:43 +0100

    Open links fullscreen in MAP formatter

--- app/views/layouts/map.blade.php
+++ app/views/layouts/map.blade.php
@@ -5,6 +5,7 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="DC.title" content="{{ $title }}"/>
+        <base target="_parent" />
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script type="text/javascript" src='{{ URL::to("js/leaflet.min.js") }}'></script>
