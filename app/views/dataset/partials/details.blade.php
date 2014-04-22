0fa37deea151823c0be6aa3ea354d0308b985ee6
Michiel Vancoillie <info@ninetynine.be>
2014-04-22 17:04:04 +0200

    Added list of formats to views

---
+++ app/views/dataset/partials/details.blade.php
@@ -0,0 +1,35 @@
+<ul class="list-group">
+    <li class="list-group-item">
+        <h5 class="list-group-item-heading">Formats</h5>
+        <div class="btn-group formats">
+            <?php $i = 0; ?>
+            @foreach($formats as $format => $extension)
+                <a href="{{ $dataset_link }}.{{ $extension }}{{ $query_string }}" class="btn @if($i == 0){{ 'btn-primary' }}@endif">
+                    @if($i == 0)
+                        @if($extension == 'map')
+                            <i class='fa fa-expand'></i>
+                        @else
+                            <i class='fa fa-file-text-o'></i>
+                        @endif
+                    @endif
+                    {{ $format }}
+                </a>
+                <?php $i++; ?>
+            @endforeach
+        </div>
+    </li>
+    @if(!empty($source_definition['description']))
+        <li class="list-group-item">
+            <h5 class="list-group-item-heading">Description</h5>
+            <p class="list-group-item-text">
+                {{ $source_definition['description'] }}
+            </p>
+        </li>
+    @endif
+    <li class="list-group-item">
+        <h5 class="list-group-item-heading">Source Type</h5>
+        <p class="list-group-item-text">
+            {{ strtoupper($source_definition['type']) }}
+        </p>
+    </li>
+</ul>
