346b1b2c364d4e146ab22c5035a5123b619dbf73
Thomas <info@thomasg.be>
2016-04-06 10:57:56 +0200

    Clean up geodcat map

--- app/views/dataset/remote.blade.php
+++ app/views/dataset/remote.blade.php
@@ -25,58 +25,48 @@
             </p>
         </li>
     </ul>
-    <div id="map" style="display: none;"></div>
+    <div id="geodcatmap" style="display: none;"></div>
 </div>
 
-<style>
-#map { width:100%; height: 200px;min-height: 200px; background: blue;margin-top: 20px; }
-@media (min-height: 500px) {
-    #map {height: 300px;}
-}
-</style>
 <link rel="stylesheet" href="{{ URL::to("css/leaflet.css") }}" />
 <script type="text/javascript" src='{{ URL::to("js/leaflet.min.js") }}'></script>
 <script textype="text/javascript" src='{{ URL::to("js/rdf2html.min.js") }}'></script>
+<script type="text/javascript" src='{{ URL::to("js/n3-browser.min.js") }}'></script>
 <script type="text/javascript">
 var dcat = {{json_encode($source_definition['dcat'])}}
+
+// Show rdf triples
 var config = {
     plugins: ['triples', 'map', 'ontology', 'paging']
 };
 rdf2html(dcat, config);
-</script>
-<script type="text/javascript" src='{{ URL::to("js/n3-browser.min.js") }}'></script>
-<script type="text/javascript">
-var dcat = {{json_encode($source_definition['dcat'])}}
+
+// Show map based on parsed turtle
 var parser = N3.Parser();
 var triples = [];
 parser.parse(dcat, function (error, triple, prefixes) {
     if (triple) {
-        triples.push(triple)
-    } else {
-        parsed()
+        return triples.push(triple)
+    }
+    for (var i = 0; i < triples.length; i++) {
+        if (triples[i].predicate !== 'http://www.w3.org/ns/locn#geometry') {
+            continue;
+        }
+        try {
+            var json = JSON.parse(N3.Util.getLiteralValue(triples[i].object));
+            showMap(json);
+            break;
+        } catch (e) {}
     }
 });
-function parsed () {
-    for (var i = 0; i < triples.length; i++) {
-        if (triples[i].predicate === 'http://www.w3.org/ns/locn#geometry') {
-                console.log(N3.Util.getLiteralValue(triples[i].object))
-            try {
-                var json = JSON.parse(N3.Util.getLiteralValue(triples[i].object));
-                showMap(json);
-                break;
-            } catch (e) {}
-        }
-    }
-}
 function showMap (json) {
-    var feature = L.geoJson(json)
-    document.querySelector('#map').style = '';
-    var map = L.map('map');
+    document.querySelector('#geodcatmap').style = '';
+    var map = L.map('geodcatmap');
     L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
         minZoom: 3
     }).addTo(map);
-    feature.addTo(map);
+    var feature = L.geoJson(json).addTo(map);
     map.fitBounds(feature.getBounds());
 }
 </script>
