dac918beb62cc80e50f5b1828334e00ddf16a68a
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2016-07-07 14:56:55 +0200

    Update filters.php

--- app/filters.php
+++ app/filters.php
@@ -13,7 +13,7 @@
 
 App::before(function ($request) {
     // Set the correct scheme, rely on the native PHP methods and some other parameters in order to figure it out
-    if (isset($_SERVER['HTTPS'])) {
+    if (!empty($_SERVER['HTTPS']) || (!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)) {
         $ssl_enabled = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443);
     } else {
         $ssl_enabled = false;
