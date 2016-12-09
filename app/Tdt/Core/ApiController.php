7a33b6da6276abf7a4b7cf6bed10f1fdb539f6e5
Wesley De Vrient <wesley@digital54.com>
2016-12-09 13:18:35 +0100

    Allow different formats on DCAT feed

--- app/Tdt/Core/ApiController.php
+++ app/Tdt/Core/ApiController.php
@@ -77,4 +77,38 @@ abstract class ApiController extends \Controller
     {
         \App::abort(405, "The HTTP method DELETE is not supported by this resource.");
     }
+
+    /**
+     * Process the URI and return the extension (=format) and the resource identifier URI
+     *
+     * @param string $uri The URI that has been passed
+     * @return array
+     */
+    public static function processURI($uri)
+    {
+        $dot_position = strrpos($uri, '.');
+
+        if (!$dot_position) {
+            return array($uri, null);
+        }
+
+        // If a dot has been found, do a couple
+        // of checks to find out if it introduces a formatter
+        $uri_parts = explode('.', $uri);
+
+        $possible_extension = strtoupper(array_pop($uri_parts));
+
+        $uri = implode('.', $uri_parts);
+
+        $formatter_class = 'Tdt\\Core\\Formatters\\' . $possible_extension . 'Formatter';
+
+        if (!class_exists($formatter_class)) {
+            // Re-attach the dot with the latter part of the uri
+            $uri .= '.' . strtolower($possible_extension);
+
+            return array($uri, null);
+        }
+
+        return array($uri, strtolower($possible_extension));
+    }
 }
