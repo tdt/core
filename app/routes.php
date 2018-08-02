37d46b6a8828290379d5730b7e43b07b30f93499
Gerald H <ghsnd@users.noreply.github.com>
2018-08-02 10:43:55 +0200

    Comment out code for future use

--- app/routes.php
+++ app/routes.php
@@ -14,7 +14,7 @@
 $env_file = dirname(__FILE__) . '/.env';
 
 // parse this as properties defined in the .env file
-$properties = parse_ini_file($env_file);
+/*$properties = parse_ini_file($env_file);
 
 $proxy_url = $properties['PROXY_URL'];
 $proxy_schema = $properties['PROXY_SCHEMA'];
@@ -25,7 +25,7 @@ if (!empty($proxy_url)) {
 
 if (!empty($proxy_schema)) {
     $proxy_schema = getenv('PROXY_SCHEMA');
-}
+}*/
 
 /**
  * Admin routes
